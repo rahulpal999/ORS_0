6d1146014e040a1241240e687a35d963f406edfd
rahulpal999 <rahul.pal566@gmail.com>
2021-04-23 16:59:45 +0530

    Project_0

---
+++ ORS_0/src/test/java/in/co/sunrays/proj0/Test/Demo.java
@@ -0,0 +1,16 @@
+package in.co.sunrays.proj0.Test;
+
+import in.co.sunrays.proj0.dto.RoleDTO;
+import in.co.sunrays.proj0.dto.UserDTO;
+
+public class Demo {
+public static void main(String[] args) {
+	String s="java";
+	String s2="java";
+	UserDTO u=new UserDTO();
+	RoleDTO dt=new RoleDTO();
+	u.equals(dt);
+	s.equals(s2);
+
+}
+}
